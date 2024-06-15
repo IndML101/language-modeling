fixed train and test error calculation

main.py
@@ -1,258 +1,105 @@
 from src.train.trainers import TransformerTrainer, TransformerDecoderTrainer, GPTTrainer
-from src.dataset.preprocess import WikiTextDataProcessor, PTBDataProcessor
+from src.dataset.preprocess import (
+    WikiTextDataProcessor,
+    PTBDataProcessor,
+    WikiText103DataProcessor,
+)
+import math
+import torch
+import numpy as np
 
 
-# To start training
-def ptb_transformer_train():
-    # vdim must be equal to d_model//n_heads
-    model_args = dict(
-        n_layers=6,
-        d_model=256,
-        max_seq_length=35,
-        dff=1024,
-        n_heads=8,
-        dropout=0.1,
-        vdim=32,
-        bias=True,
-        epsilon=1e-5,
-    )
-    TransformerTrainer(
-        text_processor=PTBDataProcessor(),
-        learning_rate=5.0,
-        batch_size=40,
-        decay_steps=1.0,
-        **model_args,
-    ).run(100)
+class ModelConfig:
+    def __init__(
+        self,
+        text_processor,
+        learning_rate,
+        batch_size,
+        decay_steps,
+        model_name=None,
+        tstamp=None,
+        **kwargs,
+    ):
+        self.__dict__.update(**kwargs)
+        self.kwargs = kwargs
+        self.text_processor = text_processor
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.decay_steps = decay_steps
+        if model_name is not None and tstamp is not None:
+            self.model_name = "Artifacts/{}-n{}-d{}-dff{}-h{}-{}.pt".format(
+                model_name,
+                self.kwargs["n_layers"],
+                self.kwargs["d_model"],
+                self.kwargs["dff"],
+                self.kwargs["n_heads"],
+                tstamp,
+            )
+        else:
+            self.model_name = None
+        
 
+    def trainllm(self, model, n_epochs):
+        last_val_loss = model._evaluate()
+        print('-'*89)
+        print('*'*40, round(math.exp(last_val_loss),2), '*'*40)
+        print('-'*89)
+        model.run(n_epochs, last_val_loss)
 
-# To continue training from last checkpoint
-def ptb_transformer_retrain(tstamp: str):
-    model_args = dict(
-        n_layers=6,
-        d_model=256,
-        max_seq_length=35,
-        dff=1024,
-        n_heads=8,
-        dropout=0.1,
-        vdim=32,
-        bias=True,
-        epsilon=1e-5,
-    )
-    model_name = "Artifacts/Transformer-n{}-d{}-dff{}-h{}-{}.pt".format(
-        model_args["n_layers"],
-        model_args["d_model"],
-        model_args["dff"],
-        model_args["n_heads"],
-        tstamp,
-    )
-    text_processor = "Artifacts/Transformer-n{}-d{}-dff{}-h{}-{}.dt".format(
-        model_args["n_layers"],
-        model_args["d_model"],
-        model_args["dff"],
-        model_args["n_heads"],
-        tstamp,
-    )
-    last_val_loss = 3.48
-    TransformerTrainer(
-        model=model_name,
-        text_processor=PTBDataProcessor(),
-        learning_rate=5.0,
-        batch_size=40,
-        decay_steps=20.0,
-        **model_args,
-    ).run(200, last_val_loss=last_val_loss)
-
-
-# To start training
-def wikitext2_transformer_train():
-    # vdim must be equal to d_model//n_heads
-    model_args = dict(
-        n_layers=4,
-        d_model=256,
-        max_seq_length=35,
-        dff=1024,
-        n_heads=8,
-        dropout=0.1,
-        vdim=32,
-        bias=True,
-        epsilon=1e-5,
-    )
-    TransformerTrainer(
-        learning_rate=5.0, batch_size=40, decay_steps=1.0, **model_args
-    ).run(100)
-
-
-# To continue training from last checkpoint
-def wikitext2_transformer_retrain(tstamp: str):
-    model_args = dict(
-        n_layers=6,
-        d_model=256,
-        max_seq_length=35,
-        dff=1024,
-        n_heads=8,
-        dropout=0.1,
-        vdim=32,
-        bias=True,
-        epsilon=1e-5,
-    )
-    model_name = "Artifacts/Transformer-n{}-d{}-dff{}-h{}-{}.pt".format(
-        model_args["n_layers"],
-        model_args["d_model"],
-        model_args["dff"],
-        model_args["n_heads"],
-        tstamp,
-    )
-    text_processor = "Artifacts/Transformer-n{}-d{}-dff{}-h{}-{}.dt".format(
-        model_args["n_layers"],
-        model_args["d_model"],
-        model_args["dff"],
-        model_args["n_heads"],
-        tstamp,
-    )
-    last_val_loss = 7.16
-    TransformerTrainer(
-        model=model_name,
-        text_processor=WikiTextDataProcessor(),
-        learning_rate=5.0,
-        batch_size=40,
-        decay_steps=1.0,
-        **model_args,
-    ).run(100, last_val_loss=last_val_loss)
-
-
-# To start training
-def ptb_pre_trained_decoder_train():
-    # vdim must be equal to d_model//n_heads
-    model_args = dict(
-        n_layers=6,
-        d_model=256,
-        max_seq_length=35,
-        dff=1024,
-        n_heads=8,
-        dropout=0.1,
-        vdim=32,
-        bias=True,
-        epsilon=1e-5,
-    )
-    TransformerDecoderTrainer(
-        text_processor=PTBDataProcessor(),
-        learning_rate=5.0,
-        batch_size=45,
-        decay_steps=1.0,
-        **model_args,
-    ).run(100)
-
-
-# To continue training from last checkpoint uncomment this line
-def ptb_pre_trained_decoder_retrain(tstamp: str):
-    model_args = dict(
-        n_layers=6,
-        d_model=256,
-        max_seq_length=35,
-        dff=1024,
-        n_heads=8,
-        dropout=0.1,
-        vdim=32,
-        bias=True,
-        epsilon=1e-5,
-    )
-    model_name = "Artifacts/PreTrainedDecoder-n{}-d{}-dff{}-h{}-{}.pt".format(
-        model_args["n_layers"],
-        model_args["d_model"],
-        model_args["dff"],
-        model_args["n_heads"],
-        tstamp,
-    )
-    text_processor = "Artifacts/PreTrainedDecoder-n{}-d{}-dff{}-h{}-{}.dt".format(
-        model_args["n_layers"],
-        model_args["d_model"],
-        model_args["dff"],
-        model_args["n_heads"],
-        tstamp,
-    )
-    last_val_loss = 6.39
-    TransformerTrainer(
-        model=model_name,
-        text_processor=PTBDataProcessor(),
-        learning_rate=1e-5,
-        batch_size=64,
-        decay_steps=20.0,
-        **model_args,
-    ).run(200, last_val_loss=last_val_loss)
-
-
-# To start training
-def ptb_gpt_train():
-    # vdim must be equal to d_model//n_heads
-    model_args = dict(
-        n_layers=12,
-        d_model=512,
-        max_seq_length=10,
-        dff=2048,
-        n_heads=8,
-        dropout=0.1,
-        vdim=64,
-        bias=True,
-        epsilon=1e-5,
-    )
-    GPTTrainer(
-        text_processor=PTBDataProcessor(),
-        learning_rate=5.0,
-        batch_size=40,
-        decay_steps=1.0,
-        **model_args,
-    ).run(100)
-
-
-# To continue training from last checkpoint
-def ptb_gpt_retrain(tstamp: str):
-    model_args = dict(
-        n_layers=12,
-        d_model=512,
-        max_seq_length=10,
-        dff=2048,
-        n_heads=8,
-        dropout=0.1,
-        vdim=64,
-        bias=True,
-        epsilon=1e-5,
-    )
-    model_name = "Artifacts/Transformer-n{}-d{}-dff{}-h{}-{}.pt".format(
-        model_args["n_layers"],
-        model_args["d_model"],
-        model_args["dff"],
-        model_args["n_heads"],
-        tstamp,
-    )
-    text_processor = "Artifacts/Transformer-n{}-d{}-dff{}-h{}-{}.dt".format(
-        model_args["n_layers"],
-        model_args["d_model"],
-        model_args["dff"],
-        model_args["n_heads"],
-        tstamp,
-    )
-    last_val_loss = 3.48
-    GPTTrainer(
-        model=model_name,
-        text_processor=PTBDataProcessor(),
-        learning_rate=5.0,
-        batch_size=40,
-        decay_steps=20.0,
-        **model_args,
-    ).run(100, last_val_loss=last_val_loss)
 
 
 if __name__ == "__main__":
-    tstamp = ""
 
-    # wikitext2_transformer_train()
-    # wikitext2_transformer_retrain(tstamp)
+    CUDA_LAUNCH_BLOCKING=1
 
-    # ptb_transformer_train()
-    # ptb_transformer_retrain(tstamp)
+    # model arguments
+    tstamp = '2024-06-15-15-15-47'
+    model_name = 'Transformer'
+    # text_processor = PTBDataProcessor()
+    text_processor = WikiTextDataProcessor()
+    # text_processor = WikiText103DataProcessor()
+    learning_rate = 5e-7
+    batch_size = 128
+    decay_steps = 5
+    n_epochs = 20
 
-    # ptb_pre_trained_decoder_train()
-    # ptb_pre_trained_decoder_retrain(tstamp)
+    trans_args = dict(
+        n_layers=6,
+        d_model=256,
+        max_seq_length=35,
+        dff=1024,
+        n_heads=8,
+        dropout=0.1,
+        vdim=32,
+        bias=True,
+        epsilon=1e-5,
+        device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    )
 
-    ptb_gpt_train()
-    # ptb_gpt_retrain(tstamp)
+    # model config
+    transformer_config = ModelConfig(
+        text_processor=text_processor,
+        learning_rate=learning_rate,
+        batch_size=batch_size,
+        decay_steps=decay_steps,
+        model_name=model_name,
+        tstamp=tstamp,
+        **trans_args,
+    )
+
+    # transformer
+    transformer = TransformerTrainer(
+        model=transformer_config.model_name,
+        text_processor=transformer_config.text_processor,
+        learning_rate=transformer_config.learning_rate,
+        batch_size=transformer_config.batch_size,
+        decay_steps=transformer_config.decay_steps,
+        **transformer_config.kwargs,
+    )
+
+    transformer_config.trainllm(transformer, n_epochs)
+    enc_text = ['What', 'a', 'day', '.']
+    dec_text = ['I', 'believe', 'it']
+    output = transformer.generate_text(enc_text, dec_text, n_tokes=200)
+
+    print(' '.join(output))

src/dataset/batches.py
@@ -1,3 +1,4 @@
+import numpy as np
 from typing import Tuple
 from torch import Tensor
 from src.base.modules import AbstractBatchLoader
@@ -13,6 +14,8 @@ class EncoderDecoderLLMBatchLoader(AbstractBatchLoader):
         seq_len = min(max_seq_len, len(data) - 1 - idx)
         source = data[idx : idx + seq_len // 2, :]
         target = data[idx + seq_len // 2 : idx + seq_len, :]
+        # indices = np.random.shuffle(np.arange(len(source[1])))
+        # return source[:,indices], target[:,indices]
         return source, target
 
     def load_decoder_batch(self, data: Tensor, idx: int, max_seq_len: int) -> Tuple[Tensor, Tensor]:
@@ -20,7 +23,8 @@ class EncoderDecoderLLMBatchLoader(AbstractBatchLoader):
         source = data[idx : idx + seq_len, :]
         target = data[idx + 1 : idx + 1 + seq_len, :]
         return source, target
-
+        # indices = np.random.shuffle(np.arange(len(source)))
+        # return source[indices,:,:], target[indices,:,:]
 
 
 class DecoderLLMBatchLoader(AbstractBatchLoader):
@@ -34,6 +38,8 @@ class DecoderLLMBatchLoader(AbstractBatchLoader):
         source = data[idx : idx + seq_len, :]
         target = data[idx + 1 : idx + 1 + seq_len, :]
         return source, target
+        # indices = np.random.shuffle(np.arange(len(source)))
+        # return source[indices,:,:], target[indices,:,:]
 
 
 class BERTBatchLoader(AbstractBatchLoader):

src/models/llms.py
@@ -57,7 +57,16 @@ class Transformer(nn.Module):
             epsilon=epsilon,
         )
         self.feed_forward = nn.Linear(d_model, n_tokens)
-        self.init_weights()
+        # self.init_weights()
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
     def init_weights(self):
         init_range = 0.1
@@ -89,7 +98,8 @@ class Transformer(nn.Module):
             .expand(enc_out.shape[0], enc_out.shape[1], enc_out.shape[-1])
         )
         dec_out = self.decoder(trg, enc_out, mask)
-        output = F.softmax(self.feed_forward(dec_out), dim=-1)
+        # output = F.softmax(self.feed_forward(dec_out), dim=-1)
+        output = self.feed_forward(dec_out)
         # print(output)
         return output
         # return self.feed_forward(dec_out)
@@ -148,7 +158,16 @@ class PreTrainedDecoder(nn.Module):
             epsilon=epsilon,
         )
         self.feed_forward = nn.Linear(d_model, n_tokens)
-        self.init_weights()
+        # self.init_weights()
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
     def init_weights(self):
         init_range = 0.1
@@ -233,7 +252,8 @@ class GPT(nn.Module):
             epsilon=epsilon,
         )
         self.feed_forward = nn.Linear(d_model, n_tokens)
-        self.init_weights()
+        # self.init_weights()
+        self.apply(self._init_weights)
 
     def init_weights(self):
         init_range = 0.1
@@ -245,6 +265,14 @@ class GPT(nn.Module):
         self.feed_forward.weight.data.uniform_(-init_range, init_range)
         # self.embedding.bias.data.zero_()
         # self.embedding.weight.data.uniform_(-init_range, init_range)s
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
     def make_decoder_mask(self):
         pass

src/modules/utils.py
@@ -117,7 +117,10 @@ class MultiHeadedAttention(nn.Module):
                 for i in range(self.n_heads)
             ]
         )
-        self.wo = nn.Linear(self.n_heads * vdim, self.d_model, bias=bias)
+        self.wo = nn.Sequential(
+            nn.Linear(self.n_heads * vdim, self.d_model, bias=bias),
+            nn.ReLU()
+        )
 
     def forward(
         self, key: Tensor, query: Tensor, value: Tensor, mask: Optional[Tensor] = None
@@ -200,7 +203,7 @@ class DecoderLayer(nn.Module):
             src + self.dropout1(self.masked_multihead_attn(src, src, src, mask))
         )
         trg = self.layer_norm2(
-            trg + self.dropout2(self.multihead_attn(memory, memory, trg))
+            trg + self.dropout2(self.multihead_attn(memory, trg, memory))
         )
         # trg = self.layer_norm2(
         #     trg + self.dropout2(self.multihead_attn(memory, memory, trg, mask))

src/train/trainers.py
@@ -1,4 +1,6 @@
 import os
+import random
+import numpy as np
 from src.base.modules import AbstractTrainers, AbstractDataProcessor
 from src.models.llms import Transformer, PreTrainedDecoder, GPT
 from src.dataset.batches import EncoderDecoderLLMBatchLoader, DecoderLLMBatchLoader
@@ -24,7 +26,9 @@ class TransformerTrainer(AbstractTrainers):
         **model_kwargs,
     ):
         super(TransformerTrainer, self).__init__()
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.__dict__.update(**model_kwargs)
+        self.kwargs = model_kwargs
+        self.device = self.kwargs['device']
         self.batch_size = batch_size
         if isinstance(text_processor, str):
             self.text_processor = self.load_text_processor(text_processor)
@@ -33,7 +37,7 @@ class TransformerTrainer(AbstractTrainers):
         train, val, test = self.text_processor.run()
         self.n_tokens = self.text_processor.get_vocab_size()
         self.model = Transformer(
-            device=self.device, n_tokens=self.n_tokens, **model_kwargs
+            n_tokens=self.n_tokens, **model_kwargs
         )
         if model is not None:
             self.model.load_state_dict(torch.load(model))
@@ -103,13 +107,18 @@ class TransformerTrainer(AbstractTrainers):
         log_interval = 100
         start_time = time.time()
 
-        num_batches = self.train.shape[0] // self.batch_size
-        for batch, i in enumerate(range(0, self.train.size(0) - 1, self.batch_size)):
+        num_batches = self.train.shape[0] // self.max_seq_length
+        for batch, i in enumerate(range(num_batches)):
+        # for batch, i in enumerate(range(0, self.train.size(0) - 1, self.batch_size)):
+            idx = np.random.randint(0, self.train.size(0)-self.max_seq_length-1)
+            # shuffle_idx = np.random.shuffle(np.arange(len(self.train))).to_list()
+            # input_data = self.train[shuffle_idx,:]
+            # random.shuffle(self.train)
             enc_src, enc_trg = self.enc_batch_loader.load(
-                self.train, i, self.max_seq_length
+                self.train, idx, self.max_seq_length
             )
             dec_src, dec_trg = self.enc_batch_loader.load_decoder_batch(
-                self.train, i + enc_src.shape[0], enc_src.shape[0]
+                self.train, idx + enc_src.shape[0], enc_src.shape[0]
             )
             dec_trg_mask = self.make_target_mask(dec_trg)
             output = self.model(
@@ -126,9 +135,10 @@ class TransformerTrainer(AbstractTrainers):
 
             self.optimizer.zero_grad()
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(self.model.parameters(), 0.5)
+            # torch.nn.utils.clip_grad_norm_(self.model.parameters(), 0.5)
             self.optimizer.step()
             step_loss = loss.item()
+            # print(step_loss)
             total_loss += step_loss
 
             if batch % log_interval == 0 and batch > 0:
@@ -147,8 +157,9 @@ class TransformerTrainer(AbstractTrainers):
     def _evaluate(self):
         self.model.eval()  # turn on evaluation mode
         total_loss = 0.0
+        num_batches = self.val.size(0)//self.max_seq_length
         with torch.no_grad():
-            for i in range(0, self.val.size(0) - 1, self.batch_size):
+            for i in range(0, self.val.size(0) - 1, self.max_seq_length):
                 enc_src, enc_trg = self.enc_batch_loader.load(
                     self.val, i, self.max_seq_length
                 )
@@ -157,6 +168,7 @@ class TransformerTrainer(AbstractTrainers):
                 )
                 dec_trg_mask = self.make_target_mask(dec_trg)
                 seq_len = enc_src.size(0)
+                # print(enc_src.shape)
                 output = self.model(
                     enc_src.to(self.device),
                     dec_src.to(self.device),
@@ -164,14 +176,12 @@ class TransformerTrainer(AbstractTrainers):
                 )
                 # output_flat = output.view(-1, ntokens)
                 target = self.make_target(dec_trg)
-                total_loss += (
-                    seq_len
-                    * self.criterion(
+                total_loss += self.criterion(
                         output.transpose(0, 1).transpose(1, -1),
                         target.to(self.device).transpose(0, 1).transpose(1, -1),
                     ).item()
-                )
-        return total_loss / (self.val.size(0) - 1)
+
+        return total_loss / num_batches
 
     def run(self, n_epochs: int = 10000, last_val_loss: float = float("inf")):
         best_val_loss = last_val_loss
@@ -217,6 +227,32 @@ class TransformerTrainer(AbstractTrainers):
 
             self.lr_scheduler.step()
         # model.load_state_dict(torch.load(best_model_params_path))
+
+    def generate_text(self, enc_text, dec_text, n_tokes=100):
+        self.model.eval()
+        with torch.no_grad():
+            enc_src = self.text_processor.process_data(enc_text).unsqueeze(1).to(self.device, dtype=torch.long)
+            dec_src = self.text_processor.process_data(dec_text).unsqueeze(1).to(self.device, dtype=torch.long)
+            tokens = list()
+
+            # print(dec_src.shape)
+
+            for i in range(n_tokes):
+                # print(i)
+                output = self.model(
+                    enc_src,
+                    dec_src,
+                    None
+                )
+
+                # print(torch.argmax(dec_src, -1).squeeze(-1))
+                output = torch.argmax(output, -1)
+                # print(output.shape)
+                tokens.append(output.squeeze(-1)[-1].item())
+                dec_src = torch.concat((dec_src, output[-1].unsqueeze(1)), 0)
+
+            return ' '.join(self.text_processor.vocab.lookup_tokens(tokens))
+
 
 
 class TransformerDecoderTrainer(TransformerTrainer):